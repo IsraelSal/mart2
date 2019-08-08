066aad4e87e8188455e7c7e1d308bcde43a3a009
Israel Salgado <israel.salgado@avanade.com>
2019-08-08 18:04:33 +0200

    #update

--- controllers/albumsController.js
+++ controllers/albumsController.js
@@ -3,7 +3,9 @@ var request = require("request");
 var Jsonindex;
 var risultato = [];
 var url = "https://mars.nasa.gov/rss/api/?feed=weather&category=insight&feedtype=json&ver=1.0";
-
+var giorni;
+var datecast;
+var d;
 request({
     url: url,
     json: true
@@ -12,13 +14,24 @@ request({
     if (!error && response.statusCode === 200) {
       risultato[0] = body["sol_keys"][0];
       risultato[1] = body[Jsonindex].First_UTC;
-      console.log(risultato.map(t=>t)); // Print the json response
+      risultato[1] = risultato[1].substring(0,10);
 
+      risultato[2] = body[Jsonindex].AT.av
+      risultato[3] = body[Jsonindex].AT.mn
+      risultato[4] = body[Jsonindex].AT.mx
+
+      d = new Date(risultato[1]).toDateString();
+      console.log(risultato.map(t=>t)); // Print the json response
     }
 });
+
 module.exports = {
   getAllAlbums(req, res) {
-    return res.render('music', { data: risultato[0] + '  ' + risultato[1] });
+    return res.render('music', { giorni: 'SOL ' + risultato[0],
+                                datecast: d,
+                                temperatura: risultato[2] + ' C°  ',
+                                massimo: ' max  ' + risultato[4] + ' C°  -  ',
+                                minimo: ' min  ' + risultato[3] + 'C°'});
     //return res.render('music', { data: risultato });
 
   },
--- controllers/genresController.js
+++ controllers/genresController.js
@@ -3,7 +3,7 @@ var request = require("request");
 var Jsonindex;
 var risultato = [];
 var url = "https://mars.nasa.gov/rss/api/?feed=weather&category=insight&feedtype=json&ver=1.0";
-
+var formula = [];
 request({
     url: url,
     json: true
@@ -13,14 +13,19 @@ request({
       risultato[0] = body[Jsonindex].AT.av
       risultato[1] = body[Jsonindex].AT.mn
       risultato[2] = body[Jsonindex].AT.mx
-      console.log(risultato.map(t=>t)); // Print the json response
+      formula[0] = (Number(risultato[0]) * 9/5 ) + 32
+      formula[1] = (Number(risultato[1]) * 9/5 ) + 32
+      formula[2] = (Number(risultato[2]) * 9/5 ) + 32
+
+      //console.log(risultato.map(t=>t)); // Print the json response
 
     }
 });
 module.exports = {
   getAllGenres(req, res) {
-    return res.render('music', { data: 'Temperatura -' +  risultato[0] + ' C° ' + '\n' + ' max -' + risultato[2]+ ' C° ' +
-    + ' min -' + risultato[1] + 'C°'
-  });
+    return res.render('music', { temperatura: formula[0] + ' F° ',
+    minimo: ' min ' + formula[1] + ' F° ',
+    massimo: ' max ' + formula[2] + ' F° '
+   });
   },
 };
